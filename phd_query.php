9e316578a163cc83e64cfae71d7d6fdc908b73de
snehalw7 <snehal.wadhwani7@gmail.com>
2016-12-30 15:18:12 +0530

    export csv added to phd query

--- phd_query.php
+++ phd_query.php
@@ -1,5 +1,8 @@
 <?php
   require("config.php");
+  $csv_hdr = "";
+  $csv_output = "";
+  $filename = "";
 ?>
 <!DOCTYPE html>
 <html>
@@ -59,11 +62,190 @@
     </div>
     </div>
 
+ <script type="text/javascript">
+            function submitForm(){
+                var form_table = document.getElementsByName("formtable")[0];
+                var form_filter = document.getElementsByName("filter")[0];
+                form_table.submit();
+                form_filer.submit();
+                return false;
+            }
+          
+        </script>
+
+<div style="float:right; overflow-x:scroll; overflow-y:scroll; width:750px; height:700px;">
+  <?php
+    if($_SERVER['REQUEST_METHOD']=='POST' && isset($_POST['search'])){
+      if(!isset($_POST['table']) || (isset($_POST['export_data']) && !isset($_POST['table']))){
+        alert("Please select the information to display.",1);
+      }
+      else{
+      $table = $_POST['table'];
+      $yoa = $_POST['yoa'];
+      $soa = $_POST['soa'];
+      $dept = $_POST['dept'];
+      $phd_type = $_POST['phd_type'];
+      $phd_status = $_POST['phd_status'];
+
+      $query = "";
+      $flag = true;
+      if(strcmp($table,"0")==0){
+        $filename .= "basic_student_info_";
+        $query = "SELECT phd_scholar.*, supervisor, DAC1, DAC2 from phd_scholar LEFT JOIN phd_thesis ON phd_scholar."."ID = phd_thesis."."ID where ";
+      }
+      else if(strcmp($table,"1")==0){
+        $filename .= "detailed_thesis_info_";
+        $query = "SELECT phd_scholar".".ID as ID, name, dept, research_area, initial_thesis_title, final_thesis_title, topic_approval_status, supervisor, DAC1, DAC2, examiner1, examiner2, qualifying_date, pre_submission_date, draft_submission_date, final_submission_date, viva_date, phd_awarded_date from phd_scholar, phd_thesis where phd_scholar.ID = phd_thesis.ID ";
+      }
+      else if(strcmp($table,"2")==0){
+        $filename .= "basic_fellowship_info_";
+        $query = "SELECT phd_scholar."."ID, name, dept, awardee_ref, phd_fellowship.type, start_date, end_date from phd_scholar, phd_fellowship, phd_thesis where phd_scholar.ID = phd_fellowship.ID AND phd_scholar.ID = phd_thesis.ID ";
+      }
+      else if(strcmp($table,"3")==0){
+        $filename .= "detailed_fellowship_info_";
+        $query = "SELECT phd_scholar".".ID, name, dept, phd_fellowship.type, financial_year, date_received, funds_total as 'Total funds', funds_fellowship as 'Fellowship funds', funds_contingency as 'Contingency funds', funds_others as 'Other funds', expenditure, balance from phd_scholar, phd_fellowship, phd_funds, phd_thesis where phd_scholar.ID = phd_fellowship.ID AND phd_scholar.ID = phd_funds.ID AND phd_scholar.ID = phd_thesis.ID ";
+      }
+      else if(strcmp($table,4)==0){
+          $filename .= "Evaluators_info_";
+          $query = "SELECT phd_scholar".".ID as ID, name, dept, supervisor, DAC1, DAC2, examiner1, examiner2 from phd_scholar, phd_thesis where phd_scholar.ID = phd_thesis.ID ";
+      }
+      else if(strcmp($table,5)==0){
+        $filename .= "Evaluator_expenses_info_";
+        $query = "SELECT phd_scholar."."ID, name, evaluator, thesis_examination_fee as 'Thesis examination fee', viva_fee as 'Viva Fee', conveyance, postal_charges as 'Postal Charges', food from phd_scholar, phd_evaluator_expenses, phd_thesis where phd_scholar.ID = phd_evaluator_expenses.ID AND phd_scholar.ID = phd_thesis.ID ";
+      }
+
+      if(!empty($yoa)){
+        $filename .= $yoa."_";
+        if(strcmp($table,"0")==0 && $flag == true){
+          $query = $query."yoa = '".$yoa."' ";
+        }
+        else{
+          $query = $query."AND yoa = '".$yoa."' ";
+        }
+        $flag=false;
+      }
+
+      if(!empty($soa)){
+        $filename .= str_ireplace(" ","_",$soa)."_";
+
+        if(strcmp($table,"0")==0 && $flag == true){
+          $query = $query."soa = '".$yoa."' ";
+        }
+        else{
+          $query = $query."AND soa = '".$yoa."' ";
+        }
+        $flag=false;
+      }
+
+      if(!empty($dept)){
+        $filename .= str_ireplace(" ","_",$dept)."_";
+        if(strcmp($table,"0")==0 && $flag == true){
+          $query = $query."dept= '".$dept."' ";
+        }
+        else{
+          $query = $query."AND dept= '".$dept."' ";
+        }
+        
+        $flag=false;
+      }
+
+      if(!empty($phd_type)){
+        $filename .= str_ireplace(" ","_",$phd_type)."_";
+        if(strcmp($table,"0")==0 && $flag == true){
+          $query  =$query."phd_scholar.type = '".$phd_type."' ";
+        }
+        else{
+        $query  =$query."AND phd_scholar.type = '".$phd_type."' ";
+        }
+        $flag=false;
+      }
+      
+      if(!empty($phd_status) && strcmp($phd_status,"all") !=0){
+        $today = date('Y-m-d');
+        if(strcmp($table,"0")==0 && $flag == true){
+          if(strcmp($phd_status,"completed")==0){
+            $filename .= "completed";
+            $query = $query."phd_awarded_date IS NOT NULL AND phd_awarded_date <= '".$today."'";
+          }
+          else{
+            $filename .= "ongoing";
+            $query = $query."(phd_awarded_date IS NULL OR phd_awarded_date > '".$today."')";
+          }
+        }
+        else{
+          if(strcmp($phd_status,"completed")==0){
+            $filename .= "completed";
+            $query = $query."AND phd_awarded_date IS NOT NULL AND phd_awarded_date <= '".$today."'";
+          }
+          else{
+            $filename .= "ongoing";
+            $query = $query."AND (phd_awarded_date IS NULL OR phd_awarded_date > '".$today."')";
+          }
+        }
+        $flag=false;
+      }
+
+      if(strcmp($table,"0")==0 && $flag == true){
+        $query = $query."1";
+      }
+      
+      $exec_query = mysqli_query($conn, $query);
+      if($exec_query){
+        $num_fields = mysqli_num_fields($exec_query);
+          echo "<div class='col-md-12'> <table class='table table-bordered table-striped' id='search_table'> <thead> <tr>";
+          for($i=0; $i<$num_fields; $i++)
+        {
+          $field = mysqli_fetch_field($exec_query);
+          echo "<th>{$field->name}</th>";
+          $csv_hdr .= $field->name . ",";
+        }
+        if(strcmp($table,"1")==0 || strcmp($table,"4")==0){
+          echo "<th>Co-supervisor1</th>"; $csv_hdr .= "Co-supervisor1, ";
+          echo "<th>Co-supervisor2</th>"; $csv_hdr .= "Co-supervisor2, ";
+          echo "<th>Co-supervisor3</th>"; $csv_hdr .= "Co-supervisor3";
+        }
+        else{
+          rtrim($csv_hdr, ",");
+        }
+        echo "</tr></thead> <tbody>";
+
+        while($row = mysqli_fetch_row($exec_query))
+        {
+          echo "<tr>";
+
+          // $row is array... foreach( .. ) puts every element
+          // of $row to $cell variable
+          foreach($row as $cell){
+            echo "<td>".$cell."</td>";
+            $csv_output .= $cell.", ";
+          }
+          if(strcmp($table,"1")==0 || strcmp($table,"4")==0){
+          $co_query = "SELECT cosupervisor from phd_cosupervisor where phd_cosupervisor."."ID = '".$row[0]."'";
+          $exec_co_query = mysqli_query($conn,$co_query);
+          if($exec_co_query){
+             while($row = mysqli_fetch_row($exec_co_query)){
+                 echo "<td>".$row[0]."</td>";
+                 $csv_output .= $row[0].", ";
+             }
+          }
+        }
+        rtrim($csv_output,", ");
+        $csv_output .= "\n";
+          echo "</tr>\n";
+        
+        }
+      }
+    }
+  }
+  ?>
+  </tbody>
+</table>
+</div>
+</div>
+
  <div class="container">
  <div style="float:left;">
 
-   
-    
     <legend>Filters</legend>
 
       <!-- Select Basic -->
@@ -158,180 +340,21 @@
   <div class="col-md-4 text-right">
     <button id="search" name="search" class="btn btn-primary" onclick="submitForm()">Search</button>
   </div>
+</form>
   <form name="export" action="export.php" method="post">
   <div class="col-md-6 text-right">
-    <input type="submit" class="btn btn-success"  value="Export table to CSV">
+    <input type="submit" class="btn btn-success"  value="Export table to CSV" name="export_data">
     </div>
     <input type="hidden" value="<? echo $csv_hdr; ?>" name="csv_hdr">
     <input type="hidden" value="<? echo $csv_output; ?>" name="csv_output">
     <input type="hidden" value="<? echo $filename; ?>" name="filename">
-</form>
+ 
+  </form>
 </div>
-
-
-</div>
- <script type="text/javascript">
-            function submitForm(){
-                var form_table = document.getElementsByName("formtable")[0];
-                var form_filter = document.getElementsByName("filter")[0];
-                form_table.submit();
-                form_filer.submit();
-                return false;
-            }
-          
-        </script>
-
-<div style="float:right; overflow-x:scroll; overflow-y:scroll; width:750px; height:700px;">
-  <?php
-    if($_SERVER['REQUEST_METHOD']=='POST' && isset($_POST['search'])){
-
-      
-
-      if(!isset($_POST['table'])){
-        alert("Please select the information to display.",1);
-        die();
-      }
-      $table = $_POST['table'];
-      $yoa = $_POST['yoa'];
-      $soa = $_POST['soa'];
-      $dept = $_POST['dept'];
-      $phd_type = $_POST['phd_type'];
-      $phd_status = $_POST['phd_status'];
-
-      $query = "";
-      $flag = true;
-      if(strcmp($table,"0")==0){
-
-        $query = "SELECT phd_scholar.*, supervisor, DAC1, DAC2 from phd_scholar LEFT JOIN phd_thesis ON phd_scholar."."ID = phd_thesis."."ID where ";
-      }
-      else if(strcmp($table,"1")==0){
-        $query = "SELECT phd_scholar".".ID as ID, name, dept, research_area, initial_thesis_title, final_thesis_title, topic_approval_status, supervisor, DAC1, DAC2, examiner1, examiner2, qualifying_date, pre_submission_date, draft_submission_date, final_submission_date, viva_date, phd_awarded_date from phd_scholar, phd_thesis where phd_scholar.ID = phd_thesis.ID ";
-      }
-      else if(strcmp($table,"2")==0){
-        $query = "SELECT phd_scholar."."ID, name, dept, awardee_ref, phd_fellowship.type, start_date, end_date from phd_scholar, phd_fellowship, phd_thesis where phd_scholar.ID = phd_fellowship.ID ";
-      }
-      else if(strcmp($table,"3")==0){
-        $query = "SELECT phd_scholar".".ID, name, dept, phd_fellowship.type, financial_year, date_received, funds_total as 'Total funds', funds_fellowship as 'Fellowship funds', funds_contingency as 'Contingency funds', funds_others as 'Other funds', expenditure, balance from phd_scholar, phd_fellowship, phd_funds, phd_thesis where phd_scholar.ID = phd_fellowship.ID AND phd_scholar.ID = phd_funds.ID ";
-      }
-      else if(strcmp($table,4)==0){
-          $query = "SELECT phd_scholar".".ID as ID, name, dept, supervisor, DAC1, DAC2, examiner1, examiner2 from phd_scholar, phd_thesis where phd_scholar.ID = phd_thesis.ID ";
-      }
-      else if(strcmp($table,5)==0){
-        $query = "SELECT phd_scholar."."ID, name, evaluator, thesis_examination_fee as 'Thesis examination fee', viva_fee as 'Viva Fee', conveyance, postal_charges as 'Postal Charges', food from phd_scholar, phd_evaluator_expenses, phd_thesis where phd_scholar.ID = phd_evaluator_expenses.ID ";
-      }
-
-      if(!empty($yoa)){
-        if(strcmp($table,"0")==0 && $flag == true){
-          $query = $query."yoa = '".$yoa."' ";
-        }
-        else{
-          $query = $query."AND yoa = '".$yoa."' ";
-        }
-        $flag=false;
-      }
-
-      if(!empty($soa)){
-        if(strcmp($table,"0")==0 && $flag == true){
-          $query = $query."soa = '".$yoa."' ";
-        }
-        else{
-          $query = $query."AND soa = '".$yoa."' ";
-        }
-        $flag=false;
-      }
-
-      if(!empty($dept)){
-        if(strcmp($table,"0")==0 && $flag == true){
-          $query = $query."dept= '".$dept."' ";
-        }
-        else{
-          $query = $query."AND dept= '".$dept."' ";
-        }
-        
-        $flag=false;
-      }
-
-      if(!empty($phd_type)){
-        if(strcmp($table,"0")==0 && $flag == true){
-          $query  =$query."phd_scholar.type = '".$phd_type."' ";
-        }
-        else{
-        $query  =$query."AND phd_scholar.type = '".$phd_type."' ";
-        }
-        $flag=false;
-      }
-
-      if(!empty($phd_status) && strcmp($phd_status,"all") !=0){
-        $today = date('Y-m-d');
-        if(strcmp($table,"0")==0 && $flag == true){
-          if(strcmp($phd_status,"completed")==0){
-            $query = $query."phd_awarded_date IS NOT NULL AND phd_awarded_date <= '".$today."'";
-          }
-          else{
-            $query = $query."(phd_awarded_date IS NULL OR phd_awarded_date > '".$today."')";
-          }
-        }
-        else{
-          if(strcmp($phd_status,"completed")==0){
-            $query = $query."AND phd_awarded_date IS NOT NULL AND phd_awarded_date <= '".$today."'";
-          }
-          else{
-            $query = $query."AND (phd_awarded_date IS NULL OR phd_awarded_date > '".$today."')";
-          }
-        }
-        $flag=false;
-      }
-
-      if(strcmp($table,"0")==0 && $flag == true){
-        $query = $query."1";
-      }
-      
-
-      $exec_query = mysqli_query($conn, $query);
-      if($exec_query){
-        $num_fields = mysqli_num_fields($exec_query);
-          echo "<div class='col-md-12'> <table class='table table-bordered table-striped' id='search_table'> <thead> <tr>";
-          for($i=0; $i<$num_fields; $i++)
-        {
-          $field = mysqli_fetch_field($exec_query);
-          echo "<th>{$field->name}</th>";
-        }
-        if(strcmp($table,"1")==0 || strcmp($table,"4")==0){
-          echo "<th>Co-supervisor1</th>";
-          echo "<th>Co-supervisor2</th>";
-          echo "<th>Co-supervisor3</th>";
-        }
-        echo "</tr></thead> <tbody>";
-
-        while($row = mysqli_fetch_row($exec_query))
-        {
-          echo "<tr>";
-
-          // $row is array... foreach( .. ) puts every element
-          // of $row to $cell variable
-          foreach($row as $cell){
-            echo "<td>".$cell."</td>";
-          }
-          if(strcmp($table,"1")==0 || strcmp($table,"4")==0){
-          $co_query = "SELECT cosupervisor from phd_cosupervisor where phd_cosupervisor."."ID = '".$row[0]."'";
-          $exec_co_query = mysqli_query($conn,$co_query);
-          if($exec_co_query){
-             while($row = mysqli_fetch_row($exec_co_query)){
-                 echo "<td>".$row[0]."</td>";
-             }
-          }
-        }
-          echo "</tr>\n";
-        }
-      }
-    }
-  ?>
-  </tbody>
-</table>
-</div>
-  </div>
 </div>
 </fieldset>
-</form>
+
+  
+
 </body>
 </html>
